4fa27298309f0ebe6c1be75f24deaa0b9a6c8bbb
João Guilherme <jaaumgui@gmail.com>
2022-09-19 12:05:53 -0300

    added function to initialize anchorpane in every gridpane grids and set an id relative to its position
    Signed-off-by: João Guilherme <jaaumgui@gmail.com>

--- src/main/java/com/chess/BoardController.java
+++ src/main/java/com/chess/BoardController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,8 +15,23 @@ public class BoardController implements Initializable {
 
     @FXML
     private ImageView boardImage;
+    @FXML
+    private GridPane gridPane;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         boardImage.setImage(new Image("file:src/main/resources/boardImage.jpg"));
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                AnchorPane anchorPane = new AnchorPane();
+                anchorPane.setId(String.valueOf((char)(65+i))+String.valueOf(8-j));
+                anchorPane.setOnMouseClicked(this::onAnchorPaneClicked);
+                gridPane.add(anchorPane, i, j);
+            }
+        }
+    }
+
+    public void onAnchorPaneClicked(MouseEvent event){
+        AnchorPane anchorPane = (AnchorPane) event.getSource();
+        System.out.println(anchorPane.getId());
     }
 }
