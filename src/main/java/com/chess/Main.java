9dec7094ed221846791f11b9a956c9ae3d821f58
João Guilherme <jaaumgui@gmail.com>
2022-09-19 11:35:59 -0300

    Board create with gridpane 8x8 and background image.
    Signed-off-by: João Guilherme <jaaumgui@gmail.com>

--- src/main/java/com/chess/Main.java
+++ src/main/java/com/chess/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("chess.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Board.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        stage.setTitle("Chess");
         stage.setScene(scene);
         stage.show();
     }
